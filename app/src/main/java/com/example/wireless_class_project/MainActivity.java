63c75699961bff861b9b91d44977c7387be37e12
BuildTools <unconfigured@null.spigotmc.org>
2019-03-06 15:26:14 +0700

    Wooooo

--- app/src/main/java/com/example/wireless_class_project/MainActivity.java
+++ app/src/main/java/com/example/wireless_class_project/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ));
+                startActivity(new Intent(MainActivity.this, HomePage.class));
             }
         });
 
