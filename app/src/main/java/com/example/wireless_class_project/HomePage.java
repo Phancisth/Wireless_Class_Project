ab98aa79ea95c2129ee2287e42acbdec00360295
BuildTools <unconfigured@null.spigotmc.org>
2019-03-06 16:20:25 +0700

    Changed ID names Secured Login Logout

--- app/src/main/java/com/example/wireless_class_project/HomePage.java
+++ app/src/main/java/com/example/wireless_class_project/HomePage.java
@@ -3,6 +3,7 @@ package com.example.wireless_class_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Intent intent = getIntent();
+    }
+
+    public void Logout(View view)
+    {
+        finishAffinity();
+        Intent intent   = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
