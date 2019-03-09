f92a8fe2b89f98cdf0a5aecb1e5d2b3a38e3fc45
BuildTools <unconfigured@null.spigotmc.org>
2019-03-09 13:14:09 +0700

    Language Button now appears near the top left of the Login page

--- app/src/main/java/com/example/wireless_class_project/MainActivity.java
+++ app/src/main/java/com/example/wireless_class_project/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.wireless_class_project;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 
@@ -19,6 +23,7 @@ import static android.graphics.ColorSpace.Model.XYZ;
 public class MainActivity extends AppCompatActivity {
     private EditText LoginUsername;
     private EditText LoginPassword;
+    private TextView Language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         LoginUsername = findViewById(R.id.Login_Username);
         LoginPassword = findViewById(R.id.Login_Password);
 
+        Language = findViewById(R.id.Language);
+        Language.setText(getIntent().getStringExtra("Current_Language"));
     }
 
     public void Login(View view)
@@ -43,16 +50,32 @@ public class MainActivity extends AppCompatActivity {
     }
     public void ChangeLanguage(View view)
     {
-        String languageToLoad  = "th-rTH";
-        Locale locale = new Locale(languageToLoad);
-        Locale.setDefault(locale);
-        Configuration config = new Configuration();
-        config.locale = locale;
-        this.getResources().updateConfiguration(config,this.getResources().getDisplayMetrics());
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        String currentLocal = Locale.getDefault().getLanguage();
+        if(currentLocal == "en") {
+            Locale myLocale = new Locale("th");
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, MainActivity.class);
+            refresh.putExtra("Current_Language","EN");
+            startActivity(refresh);
+            finish();
+        }
+        else if(currentLocal == "th")
+        {
+            Locale myLocale = new Locale("en");
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, MainActivity.class);
+            refresh.putExtra("Current_Language","TH");
+            startActivity(refresh);
+            finish();
+        }
     }
 
 }
