5564843123a5dc745b5b2065e2e488d3e551568e
DESKTOP-U8GTAEM\Purit <masterchief.purit@gmail.com>
2019-04-18 18:58:45 +0700

    Update create database

--- app/src/main/java/com/example/wireless_class_project/DatabaseHelper.java
+++ app/src/main/java/com/example/wireless_class_project/DatabaseHelper.java
@@ -16,8 +16,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "user_id_grades";
     private static final String COL1 = "ID";
     private static final String COL2 = "name";
-    private int grade1 = 0;
-
+    private static final String COL3 = "ITCS175";       //Advanced Math
+    private static final String COL4 = "ITCS200";       //Fund. Program
+    private static final String COL5 = "ITCS320";       //Discrete Structure
+    private static final String COL6 = "ITCS125";       //Applied Stat
+    private static final String COL7 = "ITCS208";       //Object Oriented Programming
+    private static final String COL8 = "ITCS211";       //Intro. to Digital System
+    private static final String COL9 = "ITCS159";       //Software Lab.
+    private static final String COL10 = "ITCS210";      //Web Programming
+    private static final String COL11 = "ITCS222";      //Computer Organization and Architect.
+    private static final String COL12 = "ITCS231";      //Data Structure and Algorithm
+    private static final String COL13 = "ITCS306";      //Numerical Method
+    private static final String COL14 = "ITCS241";      //Database Management System
+    private static final String COL15 = "ITCS323";      //Computer Data Comm.
+    private static final String COL16 = "ITCS335";      //Intro to E-Biz.
+    private static final String COL17 = "ITCS343";      //Principles of OS
+    private static final String COL18 = "ITCS381";      //Intro to Multi.
+    private static final String COL19 = "ITCS361";      //Management Info. System
+    private static final String COL20 = "ITCS371";      //Intro to Soft. Eng.
+    private static final String COL21 = "ITCS414";      //Info. Storage and Retriev.
+    private static final String COL22 = "ITCS420";      //Computer Network
+    private static final String COL23 = "ITCS443";      //Parallel and Distributed Sys.
+    private static final String COL24 = "ITCS451";      //AI
 
     public DatabaseHelper(Context context) {
         super(context, TABLE_NAME, null, 1);
@@ -26,7 +46,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL2 +" TEXT)";
+                COL2 + " TEXT, " +
+                COL3 + " INTEGER, " +
+                COL4 + " INTEGER, " +
+                COL5 + " INTEGER, " +
+                COL6 + " INTEGER, " +
+                COL7 + " INTEGER, " +
+                COL8 + " INTEGER, " +
+                COL9 + " INTEGER, " +
+                COL10 + " INTEGER, " +
+                COL11 + " INTEGER, " +
+                COL12 + " INTEGER, " +
+                COL13 + " INTEGER, " +
+                COL14 + " INTEGER, " +
+                COL15 + " INTEGER, " +
+                COL16 + " INTEGER, " +
+                COL17 + " INTEGER, " +
+                COL18 + " INTEGER, " +
+                COL19 + " INTEGER, " +
+                COL20 + " INTEGER, " +
+                COL21 + " INTEGER, " +
+                COL22 + " INTEGER, " +
+                COL23 + " INTEGER, " +
+                COL24 + " INTEGER)" ;
         db.execSQL(createTable);
     }
 
